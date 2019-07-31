5506dcdb813fc4ce5e818b219c85f3fef40ef472
Nikita Filipenko <nikitaaaaa1994@mail.ru>
2019-07-31 21:11:56 +0700

    1st commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main() {
+
+        int fd1;
+        ssize_t ret=12;
+        int offset;
+        char ch[12]="Hello world!";
+	char c;
+	fd1=open("test.txt", O_WRONLY | O_CREAT| O_TRUNC, 0600);
+	if(fd1<0)
+        {
+                printf("Error open\n");
+                return 0;
+        }
+        write(fd1, ch, ret);
+	close(fd1);
+
+	fd1=open("test.txt", O_RDONLY);
+	if(fd1<0)
+        {
+                printf("Error open\n");
+                return 0;
+        }
+	offset = lseek(fd1, 0, SEEK_END);    //end of the file
+        while (offset > 0) 
+	{
+                read(fd1, &c, 1);  //read symbol
+                write(1, &c, 1);  //write symbol
+                lseek(fd1, -2, SEEK_CUR);   //go back 
+                offset--;     
+        }
+	close(fd1);
+	return 0;
+
+}
